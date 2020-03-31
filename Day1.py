37ab35dc810775c72b718cbc0b69185d4b09ded5
ISHA-GIF <62895655+ISHA-GIF@users.noreply.github.com>
2020-03-31 23:54:46 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,12 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+a = int(input())
+b = float(input())
+c = str(input())
+sumint= i + a
+sumdouble=b+d
+sumstring =s+c
+print (sumint)
+print (sumdouble)
+print (sumstring)
